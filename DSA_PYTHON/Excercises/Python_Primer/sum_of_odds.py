d467c8d5492a54e9297a16193900e9f8cead414a
samezzz <95895530+samezzz@users.noreply.github.com>
2023-02-28 21:34:15 +0000

    documentation

--- DSA_PYTHON/Excercises/Python_Primer/sum_of_odds.py
+++ DSA_PYTHON/Excercises/Python_Primer/sum_of_odds.py
@@ -3,12 +3,20 @@ Write a short python function that takes a positive integer n and returns the su
 """
 
 def sum_of_odds(pos_int):
+  # Get a list of all the numbers smaller than the positive number
   below_int = list(range(pos_int))
+  # Before iteration sum == 0
   init_sum = 0
+  # Loop through the List and add the square of the odd numbers
   for num in below_int:
     if num % 2 == 1:
       init_sum += num ** 2
   return init_sum 
+
+"""
+def sum_of_odds(pos_int):
+  return sum(i ** 2 for i in range(pos_int) if i % 2 == 1)
+"""
 
 import unittest
 

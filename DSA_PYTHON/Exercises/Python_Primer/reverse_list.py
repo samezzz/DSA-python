613ed9a5445d9ae2b6899c95c417d9fcbc5f6001
samezzz <95895530+samezzz@users.noreply.github.com>
2023-03-04 23:53:59 +0000

    reverse list

---
+++ DSA_PYTHON/Exercises/Python_Primer/reverse_list.py
@@ -0,0 +1,3 @@
+"""
+Write a short python function that receives a list of n integers, so that the numbers are listed in the opposite order than they were before, and compare this method to an equivalent python function for doing the same thing
+"""

203465f1c8830b5819011fb49c527deac07dfe19
samezzz <95895530+samezzz@users.noreply.github.com>
2023-03-05 23:14:59 +0000

    reverse_list complete

--- DSA_PYTHON/Exercises/Python_Primer/reverse_list.py
+++ DSA_PYTHON/Exercises/Python_Primer/reverse_list.py
@@ -1,3 +1,35 @@
 """
 Write a short python function that receives a list of n integers, so that the numbers are listed in the opposite order than they were before, and compare this method to an equivalent python function for doing the same thing
 """
+
+def reverse_list(data):
+    my_list = []
+    for index, value in enumerate(data):
+        return my_list.append(data[len(data) - index - 1])
+
+reverse_list([23, 24, 25, 26])
+
+import unittest
+
+class TestReverseList(unittest.TestCase):
+    def test_reverse_list(self):
+        # Test with a list of even length
+        self.assertEqual(reverse_list([1, 2, 3, 4]), [4, 3, 2, 1])
+        
+        # Test with a list of odd length
+        self.assertEqual(reverse_list([1, 2, 3, 4, 5]), [5, 4, 3, 2, 1])
+        
+        # Test with an empty list
+        self.assertEqual(reverse_list([]), [])
+        
+        # Test with a list of one element
+        self.assertEqual(reverse_list([1]), [1])
+        
+        # Test with a list of duplicate values
+        self.assertEqual(reverse_list([1, 2, 2, 3, 3, 3]), [3, 3, 3, 2, 2, 1])
+        
+        # Test with a list of negative numbers
+        self.assertEqual(reverse_list([-1, -2, -3, -4]), [-4, -3, -2, -1])
+    
+if __name__ == "__main__":
+    unittest.main()
